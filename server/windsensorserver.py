76ab8b47451c9f5c7a968e3330e88f949da7397d
Jean-Frederic Clere <jfclere@gmail.com>
2023-06-03 20:32:12 +0100

    Port to python3

--- server/windsensorserver.py
+++ server/windsensorserver.py
@@ -15,21 +15,23 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import SocketServer
+import socketserver
 import subprocess
 import sys
 import threading
-from thread import start_new_thread
+from _thread import start_new_thread
 import time
 import RPi.GPIO as GPIO
 import math
 import time
 
+PINSENSOR=37
+
 # RPi.GPIO Layout verwenden (wie Pin-Nummern)
 GPIO.setmode(GPIO.BOARD)
 
-# Pin 3 (GPIO 0) auf Input setzen
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Pin 37 (GPIO 26) auf Input setzen
+GPIO.setup(PINSENSOR, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 imp_per_sec = 0
 actual_windspeed_msec = 0
@@ -38,7 +40,7 @@ def interrupt(val):
         global imp_per_sec
         imp_per_sec += 1
 
-GPIO.add_event_detect(3, GPIO.RISING, callback = interrupt, bouncetime = 5)
+GPIO.add_event_detect(PINSENSOR, GPIO.RISING, callback = interrupt, bouncetime = 5)
 
 def ws100_imp_to_mpersec(val):
         #y = 8E-09x5 - 2E-06x4 + 0,0002x3 - 0,0073x2 + 0,4503x + 0,11
@@ -53,7 +55,7 @@ def threadeval():
         global actual_windspeed_msec
         while 1:
                 actual_windspeed_msec = ws100_imp_to_mpersec(imp_per_sec)
-                print "actual_windspeed_msec %f" % actual_windspeed_msec
+                print("actual_windspeed_msec %f" % actual_windspeed_msec)
                 imp_per_sec = 0
                 for x in events:
                     x.set()
@@ -68,7 +70,7 @@ PORT = 2400
 '''  One instance per connection.
      Override handle(self) to customize action. '''
 
-class TCPConnectionHandler(SocketServer.BaseRequestHandler):
+class TCPConnectionHandler(socketserver.BaseRequestHandler):
     def handle(self):
         global actual_windspeed_msec
         self.event = threading.Event()
@@ -84,14 +86,14 @@ class TCPConnectionHandler(SocketServer.BaseRequestHandler):
 
 ############################################################################
 
-class Server(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+class Server(socketserver.ThreadingMixIn, socketserver.TCPServer):
     # Ctrl-C will cleanly kill all spawned threads
     daemon_threads = True
     # much faster rebinding
     allow_reuse_address = True
 
     def __init__(self, server_address, RequestHandlerClass):
-        SocketServer.TCPServer.__init__(\
+        socketserver.TCPServer.__init__(\
         self,\
         server_address,\
         RequestHandlerClass)
